ff55fdc60c03db772b39d5cfed49385b0c05a242
Matias Luduena <matiasluduena23@gmail.com>
2024-08-03 10:30:07 -0300

    add admin user

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import prisma from "@/lib/db";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const user = await prisma.user.findFirst({
+    where: {
+      email: "matiaslredes@gmail.com",
+    },
+  });
+  if (!user) {
+    await prisma.user.create({
+      data: {
+        email: "matiaslredes@gmail.com",
+        name: "Matias Luduena",
+        role: "ADMIN",
+      },
+    });
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Pagina principal</h1>
