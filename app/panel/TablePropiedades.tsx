6f80047b7eff58c6301eefb9a29ab014317dcb0e
Matias Luduena <matiasluduena23@gmail.com>
2024-08-07 15:40:47 -0300

    add pagination

--- app/panel/TablePropiedades.tsx
+++ app/panel/TablePropiedades.tsx
@@ -90,21 +90,44 @@ export function TablePropiedades<TData, TValue>({
       <div className="flex items-center justify-center space-x-2 py-4">
         <Button
           variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
+          onClick={() => table.firstPage()}
           disabled={!table.getCanPreviousPage()}
         >
-          Previous
+          {"<<"}
         </Button>
         <Button
           variant="outline"
-          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<"}
+        </Button>
+        <Button
+          variant="outline"
           onClick={() => table.nextPage()}
           disabled={!table.getCanNextPage()}
         >
-          Next
+          {">"}
         </Button>
-        {table.getPageCount()}
+        <Button
+          variant="outline"
+          onClick={() => table.lastPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {">>"}
+        </Button>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={(e) => {
+            table.setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              {pageSize}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   );
